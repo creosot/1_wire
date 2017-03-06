deddaec72719969d5aec35bfa0a03b1379464d7e
creosot <creosot@yandex.ru>
2017-03-06 16:26:57 +0300

    add usbasp

---
+++ 1_wire/Release/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+atmel_start.c
+
+driver_isr.c
+
+main.c
+
+OWIcrc.c
+
+OWIHighLevelFunctions.c
+
+OWISWBitFunctions.c
+
+OWIUARTBitFunctions.c
+
+src\ccp.S
+
+src\driver_init.c
+
+src\protected_io.S
+
